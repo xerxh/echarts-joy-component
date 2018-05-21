90983f7470bf8e899a91e5caf15f9266a53f2e2e
xerxh <xhxer512@163.com>
2018-05-21 22:43:55 +0800

    学习努力

---
+++ echarts-collection-show/js/24.js
@@ -0,0 +1,76 @@
+var myChart24 = echarts.init(document.getElementById('line24'),'macarons');
+
+option24 = {
+  macarons: true,
+  tooltip : {
+      trigger: 'axis',
+      axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+          type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+      }
+  },
+  legend: {
+      data:['直接访问', '邮件营销','联盟广告','视频广告','搜索引擎']
+  },
+  toolbox: {
+      show : true,
+      // feature : {
+      //     mark : {show: true},
+      //     dataView : {show: true, readOnly: false},
+      //     magicType : {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+      //     restore : {show: true},
+      //     saveAsImage : {show: true}
+      // }
+  },
+  calculable : true,
+  xAxis : [
+      {
+          type : 'value'
+      }
+  ],
+  yAxis : [
+      {
+          type : 'category',
+          data : ['周一','周二','周三','周四','周五','周六','周日']
+      }
+  ],
+  series : [
+      {
+          name:'直接访问',
+          type:'bar',
+          stack: '总量',
+          itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+          data:[320, 302, 301, 334, 390, 330, 320]
+      },
+      {
+          name:'邮件营销',
+          type:'bar',
+          stack: '总量',
+          itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+          data:[120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+          name:'联盟广告',
+          type:'bar',
+          stack: '总量',
+          itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+          data:[220, 182, 191, 234, 290, 330, 310]
+      },
+      {
+          name:'视频广告',
+          type:'bar',
+          stack: '总量',
+          itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+          data:[150, 212, 201, 154, 190, 330, 410]
+      },
+      {
+          name:'搜索引擎',
+          type:'bar',
+          stack: '总量',
+          itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+          data:[820, 832, 901, 934, 1290, 1330, 1320]
+      }
+  ]
+};
+                  
+
+myChart24.setOption(option24);
